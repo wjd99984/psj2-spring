8cd3df11ef3c5520e3b56837e23b0c194d616471
jung tae wook <xodnr901901@naver.com>
2024-05-24 10:36:01 +0900

    자신의 글만 수정 가능

--- src/main/java/com/prj2spring/controller/board/BoardController.java
+++ src/main/java/com/prj2spring/controller/board/BoardController.java
@@ -62,7 +62,13 @@ public class BoardController {
     }
 
     @PutMapping("edit")
-    public ResponseEntity edit(@RequestBody Board board) {
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity edit(@RequestBody Board board,
+                               Authentication authentication) {
+        if (!service.hasAccess(board.getId(), authentication)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
         if (service.validate(board)) {
             service.edit(board);
             return ResponseEntity.ok().build();
