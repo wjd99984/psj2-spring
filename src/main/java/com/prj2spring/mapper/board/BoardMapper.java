e84a9a2b626211f3933e535abff3dffee20936e6
jung tae wook <xodnr901901@naver.com>
2024-05-24 09:35:14 +0900

    게시물 보기에 작성자 별명 출력

--- src/main/java/com/prj2spring/mapper/board/BoardMapper.java
+++ src/main/java/com/prj2spring/mapper/board/BoardMapper.java
@@ -24,9 +24,13 @@ public interface BoardMapper {
     List<Board> selectAll();
 
     @Select("""
-            SELECT *
-            FROM board
-            WHERE id = #{id}
+            SELECT b.id,
+                   b.title,
+                   b.content,
+                   b.inserted,
+                   m.nick_name writer
+            FROM board b JOIN member m ON b.member_id = m.id
+            WHERE b.id = #{id}
             """)
     Board selectById(Integer id);
 
